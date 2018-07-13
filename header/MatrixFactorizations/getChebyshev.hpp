2c7d069c049d59713eccf98d61ad41acb14754d3
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2018-07-13 11:34:55 +0530

    Added skeleton file for getChebyshev

---
+++ header/MatrixFactorizations/getChebyshev.hpp
@@ -0,0 +1,15 @@
+#ifndef __getChebyshev_hpp__
+#define __getChebyshev_hpp__
+
+#include "MatrixData.hpp"
+using af::matmul;
+
+namespace MatrixFactorizer
+{
+    void getChebyshev(array& U, array& S, array& V, int rank, MatrixData M) 
+    {
+
+    }
+}
+
+#endif
